8e233a3634a4c6b2413bca1b0386d6fcf90f762c
Alvee Akash <alvee@ualberta.ca>
2021-06-29 18:41:23 -0600

    creating class

---
+++ Database.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+class Database
+{
+    public string $ConnectionString = '';
+    public mysqli $mysqli;
+
+    function __construct(string $connection)
+    {
+        $ConnectionString = $connection;
+    }
+
+    public function Execute(string $class, string $method, mixed $params) : mixed
+    {
+        try
+        {
+            $mysqli = new mysqli('localhost', 'root', 'root', 'test_db');
+            echo $mysqli;
+            
+        }
+        catch(\Exception $ex)
+        {
+            echo $ex->message;
+        }
+    }
+}
+
